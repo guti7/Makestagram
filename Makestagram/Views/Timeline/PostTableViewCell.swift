0222cd3a8fad205279c6ec5fd465ad6f9b52e28c
José Gutiérrez <guti7@users.noreply.github.com>
2016-07-02 07:03:11 -0700

    Modified key functionality
    Modified timeline query to load certain number of posts
    Used the skip and limit properties
    Used TimelineComponent

--- Makestagram/Views/Timeline/PostTableViewCell.swift
+++ Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import Parse
 import Bond
 
 class PostTableViewCell: UITableViewCell {
     
     @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likesIconImageView: UIImageView!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    var postDisposable: DisposableType?
+    var likeDisposable: DisposableType?
+    
     
     // property and property observer
     var post: Post? {
         didSet {
+            
+            // break old bindings
+            postDisposable?.dispose()
+            likeDisposable?.dispose()
+            
             if let post = post {
+                
+                postDisposable = post.image.bindTo(postImageView.bnd_image)
+                likeDisposable = post.likes.observe { (value: [PFUser]?) -> () in
+                    // keyword to avoid the extra { } of the func defenition
+                    
+                    if let value = value { // value is array of type PFUser
+                        self.likesLabel.text = self.stringFromUserList(value)
+                        self.likeButton.selected = value.contains(PFUser.currentUser()!)
+                        self.likesIconImageView.hidden = (value.count == 0)
+                    } else {
+                        // default values while waiting for Parse response
+                        self.likesLabel.text = ""
+                        self.likeButton.selected = false
+                        self.likesIconImageView.hidden = true
+                    }
+                }
+                
                 // bind the image of the post to the 'postImage' view
                 post.image.bindTo(postImageView.bnd_image)
                 // whenever the value of left hand sid eof .bindTo changes
@@ -25,6 +56,25 @@ class PostTableViewCell: UITableViewCell {
         }
     }
 
+    @IBAction func moreButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func likeButtonTapped(sender: AnyObject) {
+        post?.toggleLikePost(PFUser.currentUser()!)
+    }
+    
+    /*
+     * Generates a comma separated list of usernames from an array
+     * (e.g. "User1, User2")
+     */
+    func stringFromUserList(userList: [PFUser]) -> String {
+        let usernameList = userList.map { user in user.username! }
+        let commaSeparatedUserList = usernameList.joinWithSeparator(", ")
+        return commaSeparatedUserList
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
