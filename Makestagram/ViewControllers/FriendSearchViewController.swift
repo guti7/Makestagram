34ab92fdc6b39b326e2dc37418e12631045607b7
José Gutiérrez <guti7@users.noreply.github.com>
2016-07-03 08:52:07 -0700

    FriendSearchVieController implementation
    - import Parse
    - var users: [PFUser]?
    - var followingUsers: [PFUser]?
    - var query: PFQuery?
    - enum State
    - var state: State
    - updateList(results:error:)
    - modified viewWillAppear(animated:) to cache followingUsers

--- Makestagram/ViewControllers/FriendSearchViewController.swift
+++ Makestagram/ViewControllers/FriendSearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class FriendSearchViewController: UIViewController {
     
@@ -14,6 +15,81 @@ class FriendSearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    // Stores all the users that match the current search query
+    var users: [PFUser]?
+    
+    /*
+     This is a local cache. It stores all the users this user is following.
+     It is used to update the UI immediately upon user interaction, instead of
+     having to wait for a server response.
+    */
+    var followingUsers: [PFUser]? {
+        didSet { // property observer
+            /**
+             The list of following users may be fetched after the tableview has
+             displayed cells. In this case, we reload the data to reflect 
+             "following" status.
+             */
+            tableView.reloadData()
+        }
+    }
+    
+    // The current parse query
+    var query: PFQuery? {
+        didSet {
+            // whenever we assign a new query, cancel any previous requests. You
+            // can use oldValue to access the previous value of the property.
+            oldValue?.cancel()
+        }
+    }
+    
+    // This view can be in two different states
+    enum State {
+        case DefaultMode
+        case SearchMode
+    }
+    
+    // Whenever the state changes, perform one of 
+    // the two queries and update the list.
+    var state: State = .DefaultMode {
+        didSet {
+            switch (state) {
+            case .DefaultMode:
+                query = ParseHelper.allUsers(updateList)
+            case .SearchMode:
+                let searchText = searchBar?.text ?? ""
+                query = ParseHelper.searchUsers(searchText, completionBlock: updateList)
+            }
+        }
+    }
+    
+    // MARK: Update users (list)
+    
+    /**
+     Is called as the completion block of all queries.
+     As soon as a query completes, this method updates the Table View.
+     */
+    func updateList(results: [PFObject]?, error: NSError?) {
+        self.users = results as? [PFUser] ?? []
+        self.tableView.reloadData()
+    }
+    
+    // MARK: View Lifecyle
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        state = .DefaultMode
+        
+        // Fill the cache of a user's followees(the users this user follows)
+        ParseHelper.getFollowingUsersForUser(PFUser.currentUser()!) {
+            (results: [PFObject]?, error: NSError?) -> Void in
+            let relations = results ?? []
+            // use map to extract the User being followed from a Follow object
+            self.followingUsers = relations.map {
+                $0.objectForKey(ParseHelper.ParseFollowToUser) as! PFUser
+            }
+        }
+    }
     
 
     override func viewDidLoad() {
@@ -39,3 +115,6 @@ class FriendSearchViewController: UIViewController {
     */
 
 }
+
+// MARK: TableView Data Source
+
