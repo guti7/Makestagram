998e9fbd51fc9fc10cebbe9897779bb705cdec52
José Gutiérrez <guti7@users.noreply.github.com>
2016-07-03 08:16:34 -0700

    Implementing FriendSearchTableViewCell
    - edited misspelling errors in AppDelegate.swift
    
    - defined protocol for FriendSearchViewCellDelegate
    - added delegate variable
    - added user: PFUser?
    - added canFollow: Bool?
    - completed followButtonTapped(sender:)

--- Makestagram/Views/FriendSearch/FriendSearchTableViewCell.swift
+++ Makestagram/Views/FriendSearch/FriendSearchTableViewCell.swift
@@ -7,6 +7,15 @@
 //
 
 import UIKit
+import Parse
+
+
+// MARK: Protocols
+
+protocol FriendSearchTableViewCellDelegate: class {
+    func cell(cell: FriendSearchTableViewCell, didSelectFollowUser user: PFUser)
+    func cell(cell: FriendSearchTableViewCell, didSelectUnfollowUser user: PFUser)
+}
 
 class FriendSearchTableViewCell: UITableViewCell {
     
@@ -14,10 +23,37 @@ class FriendSearchTableViewCell: UITableViewCell {
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var followButton: UIButton!
+    weak var delegate: FriendSearchTableViewCellDelegate?
+    
+    var user: PFUser? {
+        didSet {
+            usernameLabel.text = user?.username
+        }
+    }
+    
+    var canFollow: Bool? = true {
+        didSet {
+            /*
+             Change the state of the follow buton based on wheter or not
+             it is possible to follow a user.
+            */
+            if let canFollow = canFollow {
+                followButton.selected = !canFollow
+            }
+        }
+    }
     
     
     // MARK: Actions
+    
     @IBAction func followButtonTapped(sender: AnyObject) {
+        if let canFollow = canFollow where canFollow == true {
+            delegate?.cell(self, didSelectFollowUser: user!)
+            self.canFollow = false
+        } else {
+            delegate?.cell(self, didSelectUnfollowUser: user!)
+            self.canFollow = true
+        }
     }
     
 
